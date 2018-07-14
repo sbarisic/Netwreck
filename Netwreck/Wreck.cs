ff8e59a49d809d5e3577fc0a55d3afec05c2adba
Saša Barišić <cartman300@net.hr>
2018-07-14 18:42:49 +0200

    Latest

--- Netwreck/Wreck.cs
+++ Netwreck/Wreck.cs
@@ -3,8 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
-namespace Netwreck {
-	public class Wreck {
+namespace Netwrecking {
+	public struct NetPacket {
+		public IPEndPoint Sender;
+		public byte[] RawData;
+	}
+
+	public class NetWreck {
+		public static IPEndPoint CreateEndPoint(string IP, int Port) {
+			return new IPEndPoint(IPAddress.Parse(IP), Port);
+		}
+
+		UdpClient UDP;
+		int Port;
+
+		public NetWreck(int Port) {
+			UDP = new UdpClient(Port);
+			this.Port = Port;
+		}
+
+		public void SendRaw(byte[] Data, IPEndPoint EndPoint) {
+			if (Data.Length > 512)
+				throw new Exception("Data packet too large");
+
+			UDP.Send(Data, Data.Length, EndPoint);
+		}
+
+		public void SendRaw(byte[] Data, string IP, int Port) {
+			SendRaw(Data, CreateEndPoint(IP, Port));
+		}
+
+		public NetPacket ReceiveRaw() {
+			NetPacket Packet = new NetPacket();
+			Packet.Sender = new IPEndPoint(IPAddress.Any, Port);
+			Packet.RawData = UDP.Receive(ref Packet.Sender);
+			return Packet;
+		}
 	}
 }
